38c97069fc32d51ca011d7a94df701aba114b98c
sam <samuel6249ng@gmail.com>
2025-05-31 22:05:06 +1000

    dry for layout

--- layout.php
+++ layout.php
@@ -1,31 +1,27 @@
-<!-- layout.php -->
 <!DOCTYPE html>
 <html lang="en">
-<!-- This are all the links to the css and js files -->
-    <?php include('components/links.php'); ?> 
-    <!-- This is the body of the page, you can add a class to the body to change the style of the page -->
+<?php include('components/links.php'); ?>
 <body class="page-<?php echo $page ?? 'default'; ?>">
-    <!-- This is the navigation bar of the page -->
     <?php include('components/nav.php'); ?>
-    <!-- This is the main content of the page -->
-        <?php
-        $sectionFiles = array_filter([
-            !empty($section) && file_exists($section) ? $section : null,
-            !empty($section2) && file_exists($section2) ? $section2 : null,
-            !empty($section3) && file_exists($section3) ? $section3 : null,
-            !empty($section4) && file_exists($section4) ? $section4 : null
-        ], 'is_string');
+    <?php
+    function getValidSections(...$sections) {
+        return array_filter($sections, function ($file) {
+            return !empty($file) && is_string($file) && file_exists($file);
+        });
+    }
 
-        if ($sectionFiles) {
-            foreach ($sectionFiles as $file) include $file;
-        } else {
-            echo "<p>Error: No content files found.</p>";
-            http_response_code(404);
+    $sectionFiles = getValidSections($section ?? null, $section2 ?? null, $section3 ?? null, $section4 ?? null);
+
+    if ($sectionFiles) {
+        foreach ($sectionFiles as $file) {
+            include $file;
         }
-        ?>
-    <!-- This is the footer of the page -->
+    } else {
+        echo "<p>Error: No content files found.</p>";
+        http_response_code(404);
+    }
+    ?>
     <?php include('components/footer.php'); ?>
-    <!-- This is the script of the page -->
     <?php include('components/script.php'); ?>
 </body>
 </html>
